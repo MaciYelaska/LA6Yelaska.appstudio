7717865b79961d8775081ca111a6b8d09c681aa5
MaciYelaska <maciyelaska@creighton.edu>
2021-09-29 15:14:17 -0500

    completed assignment

--- forms/newControls/newControls.js
+++ forms/newControls/newControls.js
@@ -2,6 +2,6 @@ function Switch3_onchange() {
    MsgBox "New value is " & Switch3.value
 }
 
- Checkbox2.onchange = function() {
+ checkbox2.onchange = function() {
   NSB.MsgBox("Value is "  +  checkbox2.value)
 }
